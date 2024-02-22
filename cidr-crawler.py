461d62b4b241fdd99ed9a06e9fc8ee53fa136474
Ardi Hidayanto <72587681+termireum@users.noreply.github.com>
2024-02-22 10:40:43 +0700

    Update cidr-crawler.py

--- cidr-crawler.py
+++ cidr-crawler.py
@@ -1,5 +1,32 @@
+import argparse
 import ipaddress
+import os
 
-cidr_example = ipaddress.ip_network('192.168.0.0/24')
-for ip in cidr_example:
-    print(ip)
+def save_ip_list(output_file, ip_list):
+    with open(output_file, 'w') as file:
+        for ip in ip_list:
+            file.write(str(ip) + '\n')
+
+def main():
+    # Create argument parser
+    parser = argparse.ArgumentParser(description='CIDR IP Range Crawler')
+    parser.add_argument('-c', '--cidr', help='CIDR to crawl', required=True)
+    parser.add_argument('-o', '--output', help='Name of the file to save the list of IPs', required=True)
+    args = parser.parse_args()
+
+    # Crawl list of IPs from CIDR
+    cidr = args.cidr
+    ip_list = list(ipaddress.IPv4Network(cidr))
+
+    # Get the output directory and ensure it exists
+    output_dir = os.path.dirname(args.output)
+    if output_dir and not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    # Save the list of IPs into a file
+    save_ip_list(args.output, ip_list)
+    print(f"List of IPs has been saved to file: {args.output}")
+
+if __name__ == "__main__":
+    main()
+
